c60b1a10098a878b8a4a8bd8ad5a92b220d895ad
Sakyuba Noe <398noe@gmail.com>
2022-04-27 22:20:39 +0900

    re installed packages

--- pages/api/[dynamic]/ogp.ts
+++ pages/api/[dynamic]/ogp.ts
@@ -1,4 +1,3 @@
-import { createCanvas, registerFont } from "canvas";
 import { NextApiRequest, NextApiResponse } from "next";
 import path from "path";
 import { LicenseItems } from "../../../types/licenses";
@@ -33,20 +32,16 @@ const createOgp = async (
 
     try {
         const html = await generateHTML(level, licenses);
-        //res.setHeader('Content-Type', 'text/html');
-        //res.end(html);
-        //return;
 
         // View Port setting
         const viewport: Viewport = { width: 1200, height: 630 };
-        const image = generateImage(viewport, html);
+        const image = await generateImage(viewport, html);
 
         // Content Type
         res.statusCode = 200;
         res.setHeader("Content-Type", "image/png");
         // Vercel Edge Networkのキャッシュを利用するための設定
         res.setHeader("Cache-Control", "s-maxage=31536000, stale-while-revalidate");
-
         res.end(image);
     } catch (error) {
         res.statusCode = 500;
@@ -54,64 +49,6 @@ const createOgp = async (
         res.end("Internal Error");
         console.error(error);
     }
-    /*
-    const WIDTH = 1200 as const;
-    const HEIGHT = 630 as const;
-    const DX = 0 as const;
-    const DY = 0 as const;
-    const canvas = createCanvas(WIDTH, HEIGHT);
-    const ctx = canvas.getContext("2d");
-
-    ctx.fillStyle = "#FFF";
-    ctx.fillRect(DX, DY, WIDTH, HEIGHT);
-
-    registerFont(path.resolve("./fonts/notoBlack.otf"), {
-        family: "Noto Black"
-    });
-    registerFont(path.resolve("./fonts/notoRegular.otf"), {
-        family: "Noto Regular"
-    });
-    ctx.font = "36px Noto Black";
-    ctx.fillStyle = "#1a202c";
-    ctx.textBaseline = "middle";
-
-    ctx.font = "18px Noto Regular";
-    ctx.textAlign = "right";
-    ctx.fillText("4Character Internet License Project - 4cil.ga", 1080, 600);
-
-    ctx.font = "54px Noto Black";
-    ctx.textAlign = "center";
-    ctx.fillStyle = "#1a202c";
-    ctx.fillText("N" + level[0] + "C" + level[1] + "ライセンス", 600, 120);
-    ctx.font = "24px Noto Regular";
-    ctx.fillStyle = "#718096";
-    ctx.fillText("非商用利用レベル" + level[0] + " : 商用利用レベル" + level[1], 600, 180);
-
-    ctx.fillStyle = "#1a202c";
-    ctx.font = "48px Noto Black";
-    ctx.textAlign = "center";
-    ctx.fillText("許可", 480, 300);
-    ctx.fillText("禁止", 900, 300);
-
-    ctx.textAlign = "right"
-    ctx.fillText("非商用", 200, 360);
-    ctx.fillText("商用", 200, 480);
-
-    ctx.textAlign = "center";
-    ctx.font = "24px Noto";
-    ctx.fillText(licenses[0].allow, 480, 360);
-    ctx.fillText(licenses[0].disallow, 900, 360);
-    ctx.fillText(licenses[1].allow, 480, 480);
-    ctx.fillText(licenses[1].disallow, 900, 480);
-
-    const buffer = canvas.toBuffer();
-
-    res.writeHead(200, {
-        "Content-Type": "image/png",
-        "Content-Length": buffer.length,
-    });
-    res.end(buffer, "binary");
-    */
 };
 
 export default createOgp;
--- utils/generateImage.ts
+++ utils/generateImage.ts
@@ -1,6 +1,12 @@
 import chrome from "chrome-aws-lambda";
 import core, { Viewport } from "puppeteer-core";
 
+const exePath = process.platform === 'win32'
+? 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
+: process.platform === 'linux'
+? '/usr/bin/google-chrome'
+: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
+
 export const generateImage = async (viewport: Viewport, html: string) => {
     const options = {
         args: chrome.args,
