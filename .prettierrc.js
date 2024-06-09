6d8a8735aea5314ff7d28fa8e12cda520e5481a4
SakyubaNoe <398noe@gmail.com>
2024-06-10 01:15:37 +0900

    Add prettier and update package json

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Options} */
+module.exports = {
+    plugins: ['prettier-plugin-organize-imports', "prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+	"printWidth": 140,
+	"singleQuote": true,
+	"semi": true,
+	"useTabs": true,
+	"tabWidth": 4,
+}
