1203e1ef2db858abb5aa3658e5da66c256a7f52b
SakyubaNoe <398noe@gmail.com>
2024-06-10 00:56:14 +0900

    Install tailwind css and daisyui

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,7 @@
 import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
-export default defineConfig({});
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind()]
+});
