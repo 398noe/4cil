d0c913640859db292a25040406872a338741d457
SakyubaNoe <398noe@gmail.com>
2024-06-10 17:08:29 +0900

    Add react integration on package

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()]
+  integrations: [tailwind(), icon(), react()]
 });
