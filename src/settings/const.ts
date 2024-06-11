ba10f6fe3c0e26b261cb96436cb4a458d0efee2a
SakyubaNoe <398noe@gmail.com>
2024-06-11 16:25:59 +0900

    fix base url

--- src/settings/const.ts
+++ src/settings/const.ts
@@ -1,2 +1,2 @@
-// export const BASE_URL = 'https://4cil.nyao.me';
-export const BASE_URL = 'http://localhost:4321';
+export const BASE_URL = 'https://4cil.nyao.me';
+// export const BASE_URL = 'http://localhost:4321';
