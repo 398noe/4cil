5f53cc2541706479372b543bd6f9f21b5af4ffd6
SakyubaNoe <398noe@gmail.com>
2024-06-11 17:45:55 +0900

    Add sitemap integration and config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-
 import react from "@astrojs/react";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon(), react()]
+  site: "https://4cil.nyao.me",
+  integrations: [tailwind(), icon(), react(), sitemap({
+    filter: (page) => page !== "https://4cil.nyao.me/404",
+  })]
 });
